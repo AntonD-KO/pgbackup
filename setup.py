32af7b1d6b040d040eaa08cf0b83750fc7f91676
EC2 Default User <ec2-user@ip-172-31-40-199.us-east-2.compute.internal>
2020-01-23 01:42:56 +0000

    Final Commit

--- setup.py
+++ setup.py
@@ -9,8 +9,16 @@ setup(
     authoer='Anton D-KO',
     author_email='homedyachenko@gmail.com',
     description='A Utility for backing up PostgreSQL databases.',
-    long_description=longs_description,
+    #long_description=longs_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AntonD-KO/pgbackup',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main'
+        ],
+    }
 )
